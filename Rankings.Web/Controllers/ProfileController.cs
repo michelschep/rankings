a30793cd79a3bbf917a9610f1c3ac834d4c216ad
Michel <michelschep@gmail.com>
2019-08-30 09:47:19 +0200

    Add simple/dummy form to edit profile

---
+++ Rankings.Web/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rankings.Web.Models;
+
+namespace Rankings.Web.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        public IActionResult Profile()
+        {
+            var profileViewModel = new ProfileViewModel("pietje@domain.nl", "Pietje Puk");
+            return View(profileViewModel);
+        }
+    }
+}
---
+++ Rankings.Web/Models/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Rankings.Web.Models
+{
+    public class ProfileViewModel
+    {
+        public string EmailAddress { get; }
+        public string DisplayName { get; }
+
+        public ProfileViewModel(string emailAddress, string displayName)
+        {
+            EmailAddress = emailAddress;
+            DisplayName = displayName;
+        }
+    }
+}
