5aee91799d1cdddd739761c206814d271bd663a2
Michel <michelschep@gmail.com>
2019-09-20 19:11:17 +0200

    Add basic ranking with dummy data

---
+++ Rankings.Web/Controllers/RankingsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Rankings.Core.Interfaces;
+using Rankings.Web.Models;
+
+namespace Rankings.Web.Controllers
+{
+    public class RankingsController : Controller
+    {
+        private readonly IRankingService _rankingService;
+
+        public RankingsController(IRankingService rankingService)
+        {
+            _rankingService = rankingService ?? throw new ArgumentNullException(nameof(rankingService));
+        }
+
+        public IActionResult Index()
+        {
+            List<RankingViewModel> model = new List<RankingViewModel>
+            {
+                new RankingViewModel {Points = 1200, NamePlayer = "Michel", Ranking = 1},
+                new RankingViewModel {Points = 1200, NamePlayer = "Geale", Ranking = 2},
+                new RankingViewModel {Points = 1200, NamePlayer = "Johannes", Ranking = 3},
+                new RankingViewModel {Points = 1200, NamePlayer = "Arjen", Ranking = 4},
+                new RankingViewModel {Points = 1200, NamePlayer = "Hans", Ranking = 5}
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Rankings.Web/Models/RankingViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rankings.Web.Models
+{
+    public class RankingViewModel
+    {
+        [Display(Name = "Ranking")]
+        public int Ranking { get; set; }
+
+        [Display(Name = "Player")]
+        public string NamePlayer { get; set; }
+
+        [Display(Name = "Points")]
+        public int Points { get; set; }
+    }
+}
