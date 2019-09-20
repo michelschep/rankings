ecc9f79ae61675b73f585994439193e744eade01
Michel <michelschep@gmail.com>
2019-09-20 09:52:25 +0200

    Remove dead code

--- Rankings.Infrastructure/Data/RankingContext.cs
+++ Rankings.Infrastructure/Data/RankingContext.cs
@@ -13,11 +13,5 @@ namespace Rankings.Infrastructure.Data
         public DbSet<Profile> Profiles { get; set; }
         public DbSet<GameType> GameTypes { get; set; }
         public DbSet<Game> Games { get; set; }
-
-//        protected override void OnModelCreating(ModelBuilder modelBuilder)
-//        {
-//            modelBuilder.Entity<Profile>().ToTable("Profile");
-//            modelBuilder.Entity<GameType>().ToTable("GameType");
-//        }
     }
 }
