3dbadecfdb4706808653bb7a5d971c3274677ae2
Michel <michelschep@gmail.com>
2019-10-27 13:45:20 +0100

    Add delta elo to matrix page and use current user as reference

--- Rankings.Web/Controllers/HomeController.cs
+++ Rankings.Web/Controllers/HomeController.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Metadata;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Rankings.Core.Entities;
 using Rankings.Core.Interfaces;
 using Rankings.Core.Services;
 using Rankings.Web.Models;
@@ -35,6 +41,40 @@ namespace Rankings.Web.Controllers
             return View("Index", model);
         }
         
+        public IActionResult Matrix()
+        {
+            var model = new List<EloMatrixViewModel>();
+
+            // TODO make it work for other types as well
+            // TODO maybe better some build in types if it really means something different. Or consts to avoid strings all over the place.
+            var ratings = _rankingService.Ranking("tafeltennis").OldRatings;
+            var thisPlayerElo = ratings.First(pair => pair.Key.EmailAddress == User.Identity.Name).Value.Ranking;
+            foreach (var stats in ratings.OrderByDescending(pair => pair.Value.Ranking))
+            {
+                var eloDiff = (int) Math.Round(stats.Value.Ranking - thisPlayerElo, 0, MidpointRounding.AwayFromZero);
+                var draw = CalculateDeltaResult(thisPlayerElo, stats, 1, 1);
+                var oneZeroWin = CalculateDeltaResult(thisPlayerElo, stats, 1, 0);
+                var oneZeroLost = CalculateDeltaResult(thisPlayerElo, stats, 0, 1);
+
+                var twoZeroWin = CalculateDeltaResult(thisPlayerElo, stats, 2, 0);
+                var twoZeroLost = CalculateDeltaResult(thisPlayerElo, stats, 0, 2);
+                
+                var threeZeroWin = CalculateDeltaResult(thisPlayerElo, stats, 3, 0);
+                var threeZeroLost = CalculateDeltaResult(thisPlayerElo, stats, 0, 3);
+
+                var line = new EloMatrixViewModel(stats.Key.DisplayName, (int) Math.Round(stats.Value.Ranking, 0, MidpointRounding.AwayFromZero), eloDiff, draw, oneZeroWin, oneZeroLost, twoZeroWin, twoZeroLost, threeZeroWin, threeZeroLost);                
+                model.Add(line);
+            }
+            
+            return View(model);
+        }
+
+        private int CalculateDeltaResult(decimal thisPlayerElo, KeyValuePair<Profile, PlayerStats> stats, int gameScore1, int gameScore2)
+        {
+            var deltaFirstPlayer = _rankingService.CalculateDeltaFirstPlayer(thisPlayerElo, stats.Value.Ranking, gameScore1, gameScore2);
+            return (int) Math.Round(deltaFirstPlayer, 0, MidpointRounding.AwayFromZero);
+        }
+
         public IActionResult Privacy()
         {
             return View();
@@ -45,6 +85,46 @@ namespace Rankings.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class EloMatrixViewModel
+    {
+        public string Name { get; }
+        public int Elo { get; }
+        public int EloDiff { get; }
+        public int Draw { get; }
+
+        [DisplayName("1-0")]
+        public int OneZeroWin { get; }
+        
+        [DisplayName("0-1")]
+        public int OneZeroLost { get; }
+        
+        [DisplayName("2-0")]
+        public int TwoZeroWin { get; }
+        
+        [DisplayName("0-2")]
+        public int TwoZeroLost { get; }
+        
+        [DisplayName("3-0")]
+        public int ThreeZeroWin { get; }
+        
+        [DisplayName("0-3")]
+        public int ThreeZeroLost { get; }
+
+        public EloMatrixViewModel(string name, int elo, int eloDiff, int draw, int oneZeroWin, int oneZeroLost, int twoZeroWin, int twoZeroLost, int threeZeroWin, int threeZeroLost)
+        {
+            Name = name;
+            Elo = elo;
+            EloDiff = eloDiff;
+            Draw = draw;
+            OneZeroWin = oneZeroWin;
+            OneZeroLost = oneZeroLost;
+            TwoZeroWin = twoZeroWin;
+            TwoZeroLost = twoZeroLost;
+            ThreeZeroWin = threeZeroWin;
+            ThreeZeroLost = threeZeroLost;
         }
     }
 }
