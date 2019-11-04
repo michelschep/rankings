c8a7d27ef77d26effe79a18ef9fd99b0666bbb7c
Michel <michelschep@gmail.com>
2019-11-04 16:32:42 +0100

    Boy Scout Rule

--- Rankings.Web/Models/CreateProfileViewModel.cs
+++ Rankings.Web/Models/CreateProfileViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Rankings.Core.Interfaces;
 
@@ -28,6 +29,8 @@ namespace Rankings.Web.Models
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var gameService = (IGamesService) validationContext.GetService(typeof(IGamesService));
+            if (gameService == null)
+                throw new ArgumentNullException(nameof(gameService));
 
             if (!gameService.IsDisplayNameUnique(value.ToString()))
                 return new ValidationResult("Display name should be unique");
