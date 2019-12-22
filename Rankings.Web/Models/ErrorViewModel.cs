c6eb0e243fb47d5576da4e570c4ab1df1788256b
Michel <michelschep@gmail.com>
2019-12-22 14:18:28 +0100

    Boy Scout Rule
    Back to only 4 warnings. Obsolete warnings. Part of the refactoring of statistics and ranking services

--- Rankings.Web/Models/ErrorViewModel.cs
+++ Rankings.Web/Models/ErrorViewModel.cs
@@ -1,9 +1,11 @@
 namespace Rankings.Web.Models
 {
+    // TODO do we need this class?
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
 
+        // ReSharper disable once UnusedMember.Global
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
