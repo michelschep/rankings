86e8735a7c0665520c43a9fb7d303c2e63d87a41
Michel <michelschep@gmail.com>
2020-01-07 13:49:13 +0100

    Fix build issue

--- Rankings.ConsoleApp/Program.cs
+++ Rankings.ConsoleApp/Program.cs
@@ -7,9 +7,11 @@ using Microsoft.Extensions.Logging;
 using Rankings.Core.Entities;
 using Rankings.Core.Services;
 using Rankings.Core.Services.ToBeObsolete;
+using Rankings.Core.Specifications;
 using Rankings.Infrastructure.Data;
 using Rankings.Infrastructure.Data.SqLite;
 using Serilog;
+using Serilog.Events;
 
 namespace Rankings.ConsoleApp
 {
@@ -33,6 +35,152 @@ namespace Rankings.ConsoleApp
         private class Program
         {
             private static void Main()
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Information()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .CreateLogger();
+
+                var services = new ServiceCollection();
+                services.AddLogging(configure => configure.AddSerilog());
+                var provider = services.BuildServiceProvider();
+
+                var gamesService = CreateGamesService(provider);
+                var games = gamesService.List(new GamesForPeriodSpecification("tafeltennis", DateTime.MinValue, new DateTime(2019, 12, 31)));
+               // foreach (var game in games)
+               // {
+               //     gamesService.RegisterGame(new Game()
+               //     {
+               //         Player1 = game.Player1,
+               //         Player2 = game.Player2,
+               //         Venue = game.Venue,
+               //         GameType = game.GameType,
+               //         Score1 = game.Score1,
+               //         Score2 = game.Score2,
+               //         RegistrationDate = DateTime.Now
+               //     });
+               // }
+
+
+                var calculator = new EloCalculatorVersion2020();
+
+                var x = calculator.CalculateDeltaPlayer(1220, 2000, 2, 3).Round(3);
+                var y = calculator.CalculateDeltaPlayer(1220, 2000, 1, 3).Round(3);
+                var z = calculator.CalculateDeltaPlayer(1220, 2000, 0, 3).Round(3);
+
+                Console.WriteLine($"{x} == {y} == {z}");
+
+                x = calculator.CalculateDeltaPlayer(1200, 1200, 3, 2).Round(3);
+                y = calculator.CalculateDeltaPlayer(1200, 1200, 3, 1).Round(3);
+                z = calculator.CalculateDeltaPlayer(1200, 1200, 3, 0).Round(3);
+
+                Console.WriteLine($"{x} == {y} == {z}");
+                //TestStats();
+                //ShowMatrix();
+
+                //var statsService = CreateStatisticsService();
+                Console.WriteLine("Ready");
+                Console.ReadLine();
+            }
+
+            private static void ShowMatrix()
+            {
+                var pi = (decimal) Math.PI / 2;
+
+                foreach (var x in Enumerable.Range(0, 50))
+                {
+                    var delta = (decimal) Math.Atan((double) x);
+                    Console.WriteLine($"delta({x}) = {delta}");
+                }
+
+                Console.WriteLine("...............");
+
+                var calculator = new EloCalculatorVersion2020();
+                var range1 = new List<int>() { 1200, 1500, 1700  }; //Enumerable.Range(12, 6);
+                var range2 = new List<int>() { 1300, 1705, 2000 }; //Enumerable.Range(12, 6);
+                foreach (var r1 in range1)
+                {
+                    foreach (var r2 in range2)
+                    {
+                        NewMethod(calculator, r1, r2, 3, 0);
+                        NewMethod(calculator, r1, r2, 3, 1);
+                        NewMethod(calculator, r1, r2, 3, 2);
+                        NewMethod(calculator, r1, r2, 2, 0);
+                        NewMethod(calculator, r1, r2, 2, 1);
+                        NewMethod(calculator, r1, r2, 1, 0);
+                    }
+
+                    //Console.WriteLine(".................");
+                    //foreach (var r2 in range2)
+                    //{
+                    //    NewMethod(calculator, r1, r2, 2, 0);
+                    //}
+
+                    //Console.WriteLine(".................");
+                    //foreach (var r2 in range2)
+                    //{
+                    //    NewMethod(calculator, r1, r2, 3, 0);
+                    //}
+
+
+                  //  Console.WriteLine(".................");
+
+                  //  foreach (var r2 in range2)
+                  //  {
+                  //      ShowResults2(calculator, r1, r2);
+                  //  }
+
+//                    Console.WriteLine(".................");
+//                    foreach (var r2 in range2)
+//                    {
+//                        ShowResults3(calculator, r1, r2);
+//                    }
+                }
+            }
+
+
+            private static void ShowResults(IEloCalculator calculator, in int r1, in int r2, int score1, int score2)
+            {
+                NewMethod(calculator, r1, r2, 1, 0);
+            }
+
+            private static void ShowResults2(IEloCalculator calculator, in int r1, in int r2)
+            {
+                NewMethod(calculator, r1, r2, 1, 0);
+                NewMethod(calculator, r1, r2, 2, 0);
+                NewMethod(calculator, r1, r2, 3, 0);
+                NewMethod(calculator, r1, r2, 4, 0);
+                NewMethod(calculator, r1, r2, 5, 0);
+                NewMethod(calculator, r1, r2, 6, 0);
+            }
+
+            private static void ShowResults3(IEloCalculator calculator, in int r1, in int r2)
+            {
+                NewMethod(calculator, r1, r2, 1, 0);
+                NewMethod(calculator, r1, r2, 2, 1);
+                NewMethod(calculator, r1, r2, 2, 0);
+                NewMethod(calculator, r1, r2, 3, 1);
+                NewMethod(calculator, r1, r2, 3, 2);
+                NewMethod(calculator, r1, r2, 3, 0);
+            }
+
+            private static void NewMethod(IEloCalculator calculator, int r1, int r2, int score1, int score2)
+            {
+                var result1 = calculator.CalculateDeltaPlayer(r1, r2, score1, score2).Round(2);
+                var result2 = calculator.CalculateDeltaPlayer(r2, r1, score2, score1).Round(2);
+
+                var result1b = calculator.CalculateDeltaPlayer(r1, r2, score2, score1).Round(2);
+                var result2b = calculator.CalculateDeltaPlayer(r2, r1, score1, score2).Round(2);
+
+                var expectedOutcome1 = GeneralEloCalculator.CalculateExpectationForBestOf(r1, r2, score1 + score2)
+                    .Round(2);
+
+                Console.WriteLine($"{r1}-{r2} {score1}-{score2} {result1,3}:{result2,3} ==== {score2}-{score1} {result1b,3}:{result2b,3}");
+            }
+
+            private static void TestStats()
             {
                 var statsService = CreateStatisticsService();
 
@@ -123,33 +271,23 @@ namespace Rankings.ConsoleApp
 
                     var r = Math.Sqrt(total / numberOfPlayers);
 
-                    Console.WriteLine($"{item.Key, -25}: {eloGames.Count, 5} {op, 3} {(100*op)/numberOfPlayers, 3 } {r}");
+                    Console.WriteLine(
+                        $"{item.Key,-25}: {eloGames.Count,5} {op,3} {(100 * op) / numberOfPlayers,3} {r}");
                 }
-
-
-                Console.ReadLine();
             }
 
             private static StatisticsService CreateStatisticsService()
             {
-                Log.Logger = new LoggerConfiguration()
-                    .Enrich.FromLogContext()
-                    .WriteTo.Console()
-                    .WriteTo.File(@".\ranKINGS.json", shared: true)
-                    .CreateLogger();
-
                 var services = new ServiceCollection();
-
                 services.AddLogging(configure => configure.AddSerilog());
                 var provider = services.BuildServiceProvider();
 
                 var rankingService = CreateGamesService(provider);
 
-                var eloConfiguration = new EloConfiguration(50, 400, true, 1200, 7);
+                var eloConfiguration = new EloConfiguration(50, 400, true, 1200, 5);
 
                 return new StatisticsService(rankingService, eloConfiguration,
-                    provider.GetService<ILogger<StatisticsService>>(),
-                    new EloCalculator(eloConfiguration, provider.GetService<ILogger<EloCalculator>>()));
+                    provider.GetService<ILogger<StatisticsService>>(), new EloCalculatorFactory());
             }
 
             private static GamesService CreateGamesService(ServiceProvider provider)
@@ -166,10 +304,11 @@ namespace Rankings.ConsoleApp
                 Console.WriteLine(database);
 
                 var connectionFactory = new SqLiteDatabaseConnectionFactory();
-                var sqLiteRankingContextFactory = new SqLiteRankingContextFactory(connectionFactory, provider.GetService<ILoggerFactory>());
+                var sqLiteRankingContextFactory =
+                    new SqLiteRankingContextFactory(connectionFactory, provider.GetService<ILoggerFactory>());
                 var repositoryFactory = new RepositoryFactory(sqLiteRankingContextFactory);
                 var repository = repositoryFactory.Create(database);
-                
+
                 return new GamesService(repository);
             }
         }
