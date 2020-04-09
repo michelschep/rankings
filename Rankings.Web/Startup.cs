afb70f9970c714a55e5e435e6f80d0e676457c61
Michel <michelschep@gmail.com>
2020-04-09 10:38:30 +0200

    Boy Scout rule

--- Rankings.Web/Startup.cs
+++ Rankings.Web/Startup.cs
@@ -9,8 +9,6 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Rankings.Core.Interfaces;
 using Rankings.Core.Models;
 using Rankings.Core.Services;
